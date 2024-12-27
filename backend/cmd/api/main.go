233eb7db49e1dbf49eb3a4efcadb97609dc64b0c
Alyasaf Meireles <gasafm62@gmail.com>
2024-12-27 12:43:22 -0300

    refactor: initializer routes and repository, usecases and some things.

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/Alym62/crud-korp/cmd/api/controllers"
-	"github.com/Alym62/crud-korp/internal/repository"
-	"github.com/Alym62/crud-korp/internal/usecases"
+	"github.com/Alym62/crud-korp/cmd/api/routes"
 	"github.com/Alym62/crud-korp/pkg/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// @TODO: Initializer router with gin
 	router := gin.Default()
 	router.GET("/healthy", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
@@ -19,23 +18,14 @@ func main() {
 		})
 	})
 
+	// @TODO: Database connection
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
 		fmt.Println("error connection database", err)
 	}
 
-	productRepository := repository.NewProductRepository(dbConnection)
-
-	productUseCase := usecases.NewProductUseCase(productRepository)
-
-	productController := controllers.NewProductController(productUseCase)
-
-	v1 := router.Group("/api/v1/product")
-	v1.GET("/list", productController.GetList)
-	v1.GET("/:id", productController.GetById)
-	v1.POST("/create", productController.Create)
-	v1.PUT("/update/:id", productController.Update)
-	v1.DELETE("/delete/:id", productController.DeleteById)
+	// @TODO: All router
+	routes.ProductRouter(router, dbConnection)
 
 	router.Run(":8080")
 }
---
+++ backend/cmd/api/routes/product_router.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"database/sql"
+
+	"github.com/Alym62/crud-korp/cmd/api/controllers"
+	"github.com/Alym62/crud-korp/internal/repository"
+	"github.com/Alym62/crud-korp/internal/usecases"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRouter(router *gin.Engine, dbConnection *sql.DB) {
+
+	productRepository := repository.NewProductRepository(dbConnection)
+
+	productUseCase := usecases.NewProductUseCase(productRepository)
+
+	productController := controllers.NewProductController(productUseCase)
+
+	v1 := router.Group("/api/v1/product")
+	v1.GET("/list", productController.GetList)
+	v1.GET("/:id", productController.GetById)
+	v1.POST("/create", productController.Create)
+	v1.PUT("/update/:id", productController.Update)
+	v1.DELETE("/delete/:id", productController.DeleteById)
+}
